f3843ed1e1c70e44d283148e2fbcaa05fd252fdf
MyteDDDDDDD <elvisyuan6988@gmail.com>
2017-01-16 20:25:57 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 int main(){
-	float a,b; // the two input numbers ,a for A and b for B, [0,10^38], 
+	_int64 a,b,sum; // the two input numbers ,a for A and b for B, [0,10^38], 
 				//10^38 is the max of float type
-	float sum = 0;
+	sum = 0;
 	cin >> a >> b;
 	//the largest number appears during counting is actually the result of applying bitwise operation "or" on a and b
 	//however bitwise operation could only operate on integral number and input exceeds the range of int type 
-	//
-	sum = a | b; 
+	//_int64 supports [0,10^18]
+	for(_int64 i=a;i<b;i++){
+		sum =max(sum, i| b);
+	}
 	cout << sum<<endl;
 	return 0;
 }
