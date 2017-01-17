08266e3a882fe7afacf56547c26063b84b146b86
MyteDDDDDDD <elvisyuan6988@gmail.com>
2017-01-17 17:29:23 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,46 @@
 #include<iostream>
+#include <algorithm> 
+#include <stdio.h>
 using namespace std;
 
-int main(){
-	_int64 a,b,sum; // the two input numbers ,a for A and b for B, [0,10^38], 
-				//10^38 is the max of float type
-	sum = 0;
-	cin >> a >> b;
-	//the largest number appears during counting is actually the result of applying bitwise operation "or" on a and b
-	//however bitwise operation could only operate on integral number and input exceeds the range of int type 
-	//_int64 supports [0,10^18]
-	for(_int64 i=a;i<b;i++){
-		sum =max(sum, i| b);
-	}
-	cout << sum<<endl;
+
+int main() {
+	unsigned long long a, b, sum,temp;
+	// the two input numbers ,a for A and b for B, [0,10^38], 
+	//read one input first, EOF mark location is after its conceptual location!!!!
+	cin >> a;
+	while (!cin.eof()) { //detect whether eof, eof needs a primce read to "boostrap" !!! 
+		
+		/* this one also works
+		while(true){
+		int a;
+		cin>>a;
+		if(!cin.eof())
+			break;
+		cout<<a;
+		}
+		*/
+		cin >> b;
+		
+		temp = pow(2,floor(log2(b)));
+		if (a < temp) {
+			sum = 2*temp - 1;
+			cout << sum<<endl;
+		}
+		else {
+			sum = b;
+			for (unsigned long long i = a; i < b; i++) {
+				sum = max(sum, i | (i + 1)); // max() is under <algorithm> header
+			}
+			cout << sum<<endl;
+		}
+	
+		
+		//the largest number appears during counting is actually the result of applying bitwise operation "or" on a and b
+		//however bitwise operation could only operate on integral number and input exceeds the range of int type 
+		//_int64, unsigned long long supports 8bytes ,range of value is [0,10^18]
+		
+		cin >> a;
+		}
 	return 0;
 }
